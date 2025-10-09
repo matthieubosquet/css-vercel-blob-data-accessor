b9864a8be85e2df5ca7a2d9037d8f16de9f55847
Matthieu Bosquet <matthieubosquet@gmail.com>
2025-10-09 01:08:25 +0100

    Cleanup blob identifier mapper

--- src/BlobIdentifierMapper.ts
+++ src/BlobIdentifierMapper.ts
@@ -1,6 +1,6 @@
 import type { FileIdentifierMapper, ResourceLink, ResourceIdentifier } from '@solid/community-server';
 
-import { BadRequestHttpError, decodeUriPathComponents, isContainerIdentifier, NotFoundHttpError, trimTrailingSlashes } from '@solid/community-server';
+import { BadRequestHttpError, decodeUriPathComponents, NotFoundHttpError, trimTrailingSlashes } from '@solid/community-server';
 import { getLoggerFor } from 'global-logger-factory';
 
 export class BlobIdentifierMapper implements FileIdentifierMapper {
@@ -17,46 +17,18 @@ export class BlobIdentifierMapper implements FileIdentifierMapper {
         throw new Error("fim mapFilePathToUrl not implemented");
     }
 
-    /**
-     * 
-     */
     public async mapUrlToFilePath(identifier: ResourceIdentifier, isMetadata: boolean, contentType?: string): Promise<ResourceLink> {
         let path = this.getRelativePath(identifier);
+
         if (isMetadata) {
             path += this.metadataSuffix;
         }
+
         this.validateRelativePath(path, identifier);
 
-        // If it's a container
-        if (isContainerIdentifier(identifier)) {
-            this.logger.debug(`URL ${identifier.path} points to the container ${path}`);
-            return { identifier, filePath: path, isMetadata: path.endsWith(this.metadataSuffix) };
-        }
-
-        // If it's a document
-        this.logger.debug(`The path for ${identifier.path} is ${path}`);
+        this.logger.debug(`The blob resource for ${identifier.path} is ${path}`);
         return { identifier, filePath: path, contentType, isMetadata: path.endsWith(this.metadataSuffix) };
-
-
-
-        // this.logger.info("identifier " + identifier.path);
-        // const a = identifier.path;
-        // const b = new URL(a);
-        // const c = b.pathname;
-        // this.logger.info("identifier pathname " + c);
-
-        // const d = c + (isMetadata ? ".meta" : "");
-        // const e = "x" + d;
-
-        // this.logger.info("identifier is " + e);
-
-        // return {
-        //     identifier: identifier,
-        //     filePath: e,
-        //     isMetadata: isMetadata
-        // };
     }
-
 
     private getRelativePath(identifier: ResourceIdentifier): string {
         if (!identifier.path.startsWith(this.baseRequestURI)) {
